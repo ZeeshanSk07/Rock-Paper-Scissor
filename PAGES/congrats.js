f7baebd18f42cc8963f563064689de9c75b0b5c5
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-03-14 12:30:27 +0530

    error fix

---
+++ PAGES/congrats.js
@@ -0,0 +1,15 @@
+function ruleshow() {
+    document.getElementById("rulebook").style.display = "block";
+  }
+
+  function playagain() {
+  
+    window.location.href ='/index.html';
+    
+   
+  }
+
+  function hiderulebook() {
+    document.getElementById("rulebook").style.display = "none";
+  }
+  
--- script.js
+++ script.js
@@ -1,141 +1,142 @@
-let user = document.getElementById('user')
-let pc = document.getElementById('pc')
-let result = document.getElementById('result')
+let user = document.getElementById("user");
+let pc = document.getElementById("pc");
+let result = document.getElementById("result");
 let yourscore = 0;
-let aiscore = 0 ;
+let aiscore = 0;
 
 // Checking if there is a score stored in local storage
-if (localStorage.getItem('yourscore')) {
-    
-    yourscore = parseInt(localStorage.getItem('yourscore'));
-    document.getElementById('user-score').textContent = yourscore;
+if (localStorage.getItem("yourscore")) {
+  yourscore = parseInt(localStorage.getItem("yourscore"));
+  document.getElementById("user-score").textContent = yourscore;
 } else {
-    
-
-    localStorage.setItem('yourscore', yourscore);
+  localStorage.setItem("yourscore", yourscore);
 }
 
-if (localStorage.getItem('aiscore')) {
-    
-    aiscore = parseInt(localStorage.getItem('aiscore'));
-    document.getElementById('pc-score').textContent = aiscore;
+if (localStorage.getItem("aiscore")) {
+  aiscore = parseInt(localStorage.getItem("aiscore"));
+  document.getElementById("pc-score").textContent = aiscore;
 } else {
-
-    localStorage.setItem('aiscore', aiscore);
+  localStorage.setItem("aiscore", aiscore);
 }
 
+function choose(input) {
+  //display img on onclick
+  user.innerHTML =
+    '<div class="' +
+    input +
+    '"><img src="images/icon-' +
+    input +
+    '.png" alt="' +
+    input +
+    '"></div>';
+  //remove margin-top from paper to display it in center and scale it.
+  var userImage = document.getElementById("user").querySelector("img");
+  userImage.style = "margin-top : 0; transform : scale(1.5);";
 
-function choose(input){
-    //display img on onclick
-    user.innerHTML =  '<div class="' + input + '"><img src="images/icon-' + input + '.png" alt="' + input + '"></div>'
-    //remove margin-top from paper to display it in center and scale it.
-    var userImage = document.getElementById('user').querySelector('img');
-    userImage.style = "margin-top : 0; transform : scale(1.5);";
+  //to display pc choice
+  const choice = ["rock", "scissor", "paper"];
+  const pcchoice = Math.floor(Math.random() * 3);
+  let pcchoose = choice[pcchoice];
 
-    //to display pc choice
-    const choice = ['rock', 'scissor', 'paper']
-    const pcchoice = Math.floor(Math.random() * 3)
-    let pcchoose = choice[pcchoice]
+  pc.innerHTML =
+    '<div class="' +
+    pcchoose +
+    '"><img src="images/icon-' +
+    pcchoose +
+    '.png" alt="' +
+    pcchoose +
+    '"></div>';
 
-    
-    pc.innerHTML =  '<div class="' + pcchoose + '"><img src="images/icon-' + pcchoose + '.png" alt="' + pcchoose + '"></div>'
+  //remove margin-top from paper to display it in center and scale it.
+  var pcImage = document.getElementById("pc").querySelector("img");
+  pcImage.style = "margin-top : 0; transform : scale(1.5);";
 
-    //remove margin-top from paper to display it in center and scale it.
-    var pcImage = document.getElementById('pc').querySelector('img');
-     pcImage.style = "margin-top : 0; transform : scale(1.5);";
+  //fix rules button in bottom after onclk
+  let rulesButtons = document.getElementsByClassName("rules-button");
 
+  for (let i = 0; i < rulesButtons.length; i++) {
+    rulesButtons[i].style.marginTop = "15.25vh";
+  }
 
-    //fix rules button in bottom after onclk
-    let rulesButtons = document.getElementsByClassName('rules-button');
+  //hide starting page and show display
+  show("play");
+  hide("game");
+  hide("rulebook");
 
-    
-    for (let i = 0; i < rulesButtons.length; i++) {
-        rulesButtons[i].style.marginTop = '15.25vh';
-    }
-
-    //hide starting page and show display 
-    show('play');
-    hide('game');
-    hide('rulebook')
-
-    //Game Logic
-if (input == pcchoose){
-    result.innerHTML = '<div>tie</div>';
-}
-else if ( 
-    (input == 'rock' && pcchoose == 'scissor') ||
-    (input == 'paper' && pcchoose == 'rock') ||
-    (input == 'scissor' && pcchoose == 'paper')
-){
-    setTimeout(function() {
-        
-    }, 1000);
-    result.innerHTML = '<div>you win</div>';
+  //Game Logic
+  if (input == pcchoose) {
+    result.innerHTML = "<div>tie</div>";
+  } else if (
+    (input == "rock" && pcchoose == "scissor") ||
+    (input == "paper" && pcchoose == "rock") ||
+    (input == "scissor" && pcchoose == "paper")
+  ) {
+    setTimeout(function () {}, 1000);
+    result.innerHTML = "<div>you win</div>";
     //show next button on win
-    document.getElementById('nextbtn').style.display="block";
+    document.getElementById("nextbtn").style.display = "block";
 
-    setTimeout(function() {
-        
-    }, 2000);
+    setTimeout(function () {}, 2000);
     //apply styles
-    userImage.style = 'margin-top:0;z-index:-1;transform:scale(1.5);box-shadow: 0 0 0 30px rgba(39,155,39), 0 0 0 45px rgba(29, 168, 43, 0.79), 0 0 0 60px rgba(46, 154, 37, 0.39);';
+    userImage.style =
+      "margin-top:0;z-index:-1;transform:scale(1.5);box-shadow: 0 0 0 30px rgba(39,155,39), 0 0 0 45px rgba(29, 168, 43, 0.79), 0 0 0 60px rgba(46, 154, 37, 0.39);";
 
     //increase score
-    yourscore +=1;
-    localStorage.setItem('yourscore', yourscore)
-    let userscore = document.getElementById('user-score');
+    yourscore += 1;
+    localStorage.setItem("yourscore", yourscore);
+    let userscore = document.getElementById("user-score");
     userscore.textContent = yourscore;
-    console.log(yourscore)
-    
-    
-}
-
-else{
-    result.innerHTML = '<div>you lost</div>';
+    console.log(yourscore);
+  } else {
+    result.innerHTML = "<div>you lost</div>";
 
     //apply styles
-    pcImage.style = 'margin-top:0;z-index:-1;transform:scale(1.5);box-shadow: 0 0 0 30px rgba(39,155,39), 0 0 0 45px rgba(29, 168, 43, 0.79), 0 0 0 60px rgba(46, 154, 37, 0.39);';
-    
+    pcImage.style =
+      "margin-top:0;z-index:-1;transform:scale(1.5);box-shadow: 0 0 0 30px rgba(39,155,39), 0 0 0 45px rgba(29, 168, 43, 0.79), 0 0 0 60px rgba(46, 154, 37, 0.39);";
+
     //increase score
-    aiscore +=1
-    localStorage.setItem('aiscore', aiscore)
-    let pcscore = document.getElementById('pc-score');
+    aiscore += 1;
+    localStorage.setItem("aiscore", aiscore);
+    let pcscore = document.getElementById("pc-score");
     pcscore.textContent = aiscore;
-    
-    
-}
+  }
 }
 
-function playagain(){
-    show('game')
-    hide('play')
-    hide('rulebook')
-    pc.innerHTML='';
-    //hide next button 
-    document.getElementById('nextbtn').style.display="none";
-        //fix rules button to original position
-        let rulesButtons = document.getElementsByClassName('rules-button');
+function playagain() {
+  show("game");
+  hide("play");
+  hide("rulebook");
 
-    
-        for (let i = 0; i < rulesButtons.length; i++) {
-            rulesButtons[i].style.marginTop = '10vh';
-        }
+  window.location.href ='./index.html';
+  pc.innerHTML = "";
+  //hide next button
+  document.getElementById("nextbtn").style.display = "none";
+  //fix rules button to original position
+  let rulesButtons = document.getElementsByClassName("rules-button");
+
+  for (let i = 0; i < rulesButtons.length; i++) {
+    rulesButtons[i].style.marginTop = "10vh";
+  }
 }
 
-function ruleshow(){
-    document.getElementById('rulebook').style.display = 'block';
+function next(){
+    window.location.href = './PAGES/congrats.html';
 }
 
-function hiderulebook(){
-    document.getElementById('rulebook').style.display ='none';
+function ruleshow() {
+  document.getElementById("rulebook").style.display = "block";
+}
+
+function hiderulebook() {
+  document.getElementById("rulebook").style.display = "none";
 }
 
 //to show element with id
-function show(id){
-    document.getElementById(id).style = "display:flex; flex-direction: row";
+function show(id) {
+  document.getElementById(id).style = "display:flex; flex-direction: row";
 }
 
 //to hide element with id
-function hide(id){
-    document.getElementById(id).style.display = "none";
+function hide(id) {
+  document.getElementById(id).style.display = "none";
 }
